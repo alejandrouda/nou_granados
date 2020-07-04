ec1f6a51edba9a0f1bb2238a5b2f42cf232f4609
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-04 19:07:14 +0200

    index menu created

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,13 @@
 class MenusController < ApplicationController
+    def index
+        @menus = Menu.all
+    end
+
+    def show
+        @meals = Meal.all
+        @drinks = Drink.all
+    end
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,25 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 p "Deleting Users..."
- User.delete_all
+User.delete_all
 
- p "Creating Users..."
- admin = User.new(
-        email: "admin@9granados.com",
-        password: "el9admin333"
- )
+p "Creating Users..."
+admin = User.new(
+       email: "admin@9granados.com",
+       password: "el9admin333"
+)
 
- admin.save
- p admin
- p "Users created..."
- p "Seeds completed!"
+admin.save
+p admin
+p "Users created..."
+
+p "Creating menus..."
+menu_mediodia = Menu.new(
+       name: "Menú del medio día",
+       price: 12
+)
+menu_mediodia.save
+p menu_mediodia
+p "Menus created..."
+
+p "Seeds completed!"
