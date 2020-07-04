c7c276a0b825909685ec12839a2bfc13a849595a
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-04 19:19:33 +0200

    public show and index for menus

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,9 +1,12 @@
 class MenusController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index, :show]
+
     def index
         @menus = Menu.all
     end
 
     def show
+        skip_before_action :authenticate_user!
         @meals = Meal.all
         @drinks = Drink.all
     end
