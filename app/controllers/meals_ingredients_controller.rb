bf1ae68159800f4fdf53acbc6befc95fc534d1a0
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-04 18:32:44 +0200

    controllers and routes created

---
+++ app/controllers/meals_ingredients_controller.rb
@@ -0,0 +1,2 @@
+class MealsIngredientsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :menus do
+    resources :meals do
+      resources :allergens, only: [:create, :index, :show]
+      resources :meals_ingredients, only: [:create, :index, :show]
+    end
+    resources :drinks do
+      resources :drinks_ingredients, only: [:create, :index, :show]
+    end
+  end
 end
