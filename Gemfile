a45b3c90cff2a7de1263735f182a5f1fbac6d75d
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-08 17:02:37 +0200

    menus added

--- Gemfile
+++ Gemfile
@@ -30,9 +30,12 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 
+gem 'cloudinary', '~> 1.12.0'
+
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass'
 gem 'simple_form'
+gem 'dotenv-rails', groups: [:development, :test]
 group :development, :test do  gem 'pry-byebug'
   gem 'pry-rails'
   gem 'dotenv-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ require 'open-uri'
 
 p "Deleting Users..."
 User.delete_all
+p "Deleting Menus"
+Menu.delete_all
 
 p "Creating Users..."
 admin = User.new(
@@ -11,16 +13,41 @@ admin = User.new(
 
 admin.save
 p admin
+
 p "Users created..."
 
 p "Creating menus..."
+
 menu_mediodia = Menu.new(
-       name: "Menú del medio día",
-       price: 12
+              name: "Menú del medio día",
+              price: 12
+       )
+       menu_mediodia.photo.attach(io: URI.open("https://res.cloudinary.com/dfyhqslry/image/upload/v1594164128/Nou%20granados/Background/background_2_boyxnp.jpg"), filename: 'background_2_boyxnp.jpg', content_type: 'jpg')
+       menu_mediodia.save
+       p menu_mediodia
+
+menu_9granados = Menu.new(
+       name: "Menú 9 granados",
 )
-menu_mediodia.photo.attach(io: URI.open("https://res.cloudinary.com/dfyhqslry/image/upload/v1594164128/Nou%20granados/Background/background_2_boyxnp.jpg"), filename: 'background_2_boyxnp.jpg', content_type: 'jpg')
-menu_mediodia.save
-p menu_mediodia
+menu_9granados.photo.attach(io: URI.open("https://res.cloudinary.com/dfyhqslry/image/upload/v1594164128/Nou%20granados/Background/background_2_boyxnp.jpg"), filename: 'background_2_boyxnp.jpg', content_type: 'jpg')
+menu_9granados.save
+p menu_9granados
+
+bodega = Menu.new(
+       name: "Bodega",
+)
+bodega.photo.attach(io: URI.open("https://res.cloudinary.com/dfyhqslry/image/upload/v1594164128/Nou%20granados/Background/background_2_boyxnp.jpg"), filename: 'background_2_boyxnp.jpg', content_type: 'jpg')
+bodega.save
+p bodega
+
+carta = Menu.new(
+       name: "Carta",
+)
+carta.photo.attach(io: URI.open("https://res.cloudinary.com/dfyhqslry/image/upload/v1594164128/Nou%20granados/Background/background_2_boyxnp.jpg"), filename: 'background_2_boyxnp.jpg', content_type: 'jpg')
+carta.save
+p carta
+
+
 p "Menus created..."
 
 p "Seeds completed!"
