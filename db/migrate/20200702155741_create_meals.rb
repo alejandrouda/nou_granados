db010f6a626729eba3109d6fd2a8c5ed4961d70d
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-16 14:16:40 +0200

    finished

--- db/migrate/20200702155741_create_meals.rb
+++ db/migrate/20200702155741_create_meals.rb
@@ -2,7 +2,7 @@ class CreateMeals < ActiveRecord::Migration[6.0]
   def change
     create_table :meals do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.text :description
       t.references :menu, null: false, foreign_key: true
 
