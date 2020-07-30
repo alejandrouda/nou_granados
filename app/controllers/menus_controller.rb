0c3ab630cbfe72cbaa0ba6321e7621011ae21e05
Alejandro Udaquiola <alejandroudaquiola1@gmail.com>
2020-07-30 17:32:42 +0200

    Menu medio dia only displays monday to friday

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -3,6 +3,15 @@ class MenusController < ApplicationController
 
     def index
         @menus = Menu.all
+        # @days = ["Monday", "Tuesday","Wednesday","Thursday","Friday"]
+        # @include = @days.include? Date.today.strftime("%A")
+        # @menus.each do |menu|
+        #     if menu.medio_dia_type == true && @include == true
+        #             menu.active = true
+        #         else
+        #            menu.active = false
+        #     end
+        # end
     end
 
     def show
